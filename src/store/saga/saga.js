6c2365d099208c104d52ad4d21f34fa60dd70680
Anastasia Nagornaya <a.nagornaya@leviossa.net>
2020-03-05 20:34:16 +0300

    updated updateUser saga

--- src/store/saga/saga.js
+++ src/store/saga/saga.js
@@ -1,49 +1,35 @@
 import { put, takeLatest, call } from "redux-saga/effects";
 import * as actions from "../actions/user";
 import * as actionCreators from "../action-creators/user";
-import { db } from "../../App";
 import rsf from "../saga-firebase";
 
 export function* getUser() {
-  //?
-  // let response =   yield db.collection("users").get();
-  const snapshot = yield call(rsf.firestore.getCollection, 'users');
+  try {
+    const snapshot = yield call(rsf.firestore.getCollection, 'users');
   
-  let users;
-  snapshot.forEach(user => {
-      users = {
-        ...users,
-        [user.id]: user.data()
-      }
-  });
-  console.log('users', users)
-  // yield fetch("https://", {
-  //   method: "GET",
-  //   headers: new Headers({
-  //     "access-token": localStorage.getItem("access-token"),
-  //     client: localStorage.getItem("client"),
-  //     uid: localStorage.getItem("uid")
-  //   })
-  // });
-  // let data = yield response.json();
+    let users = [];
+    snapshot.forEach(user => {
+        users.push({docId: user.id, ...user.data()});
+    });
+  
+    console.log('users', users)
+  } catch (error) {
+    console.log('getUser error: ', error)
+  }
   // yield put(actionCreators.getUserSuccess);
-
-  // .then(response => )
 }
 
-function* updateUser(action) {
-  let response = yield fetch(`https://${action.body.id}`, {
-    method: "PUT",
-    body: JSON.stringify(action.body.message),
-    headers: new Headers({
-      "content-type": "application/json",
-      "access-token": localStorage.getItem("access-token"),
-      client: localStorage.getItem("client"),
-      uid: localStorage.getItem("uid")
-    })
-  });
-  let data = yield response.json();
-  yield put(actionCreators.updateUserSuccess);
+function* updateUser(id, data) {
+  try {
+    yield call(
+      rsf.firestore.updateDocument,
+      `users/${id}`,
+      ...data,
+    );
+  } catch (error) {
+    console.log('updateUser error: ', error)
+  }
+  // yield put(actionCreators.updateUserSuccess);
 }
 
 export default function* userSaga() {
